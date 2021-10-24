9559d88ef0c5d2b170f4d5669ff228352a9ba9f1
Tayna <reistayna@hotmail.com>
2021-10-24 10:34:48 -0300

    Calculadora implementada

--- codigo/Calculadora.py
+++ codigo/Calculadora.py
@@ -1,1 +1,24 @@
-print('Hello World')
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.b = num2
+        self.a = num1
+
+    def soma(self):
+        return self.a + self.b
+
+    def subtracao(self):
+        return self.a - self.b
+
+    def divisao(self):
+        return self.a / self.b
+
+    def multiplicacao(self):
+        return self.a * self.b
+
+
+if __name__ == '__main__':
+    calculadora = Calculadora(10, 2)
+    print(calculadora.soma())
+    print(calculadora.subtracao())
+    print(calculadora.divisao())
+    print(calculadora.multiplicacao())
